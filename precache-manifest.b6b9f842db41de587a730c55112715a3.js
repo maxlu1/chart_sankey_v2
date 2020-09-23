73e1c2a37e72ed8689b885a4ad7816c530e0d47e
maxlu1 <43302103+maxlu1@users.noreply.github.com>
2020-09-23 10:52:25 -0300

    Upload test files.
    Upload test files.

---
+++ precache-manifest.b6b9f842db41de587a730c55112715a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac8749c09687558052cfc68a25aebda",
+    "url": "/chart_sankey_v2/index.html"
+  },
+  {
+    "revision": "2780c184026116d70610",
+    "url": "/chart_sankey_v2/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "87c672249f6f92576e74",
+    "url": "/chart_sankey_v2/static/css/main.0070c72c.chunk.css"
+  },
+  {
+    "revision": "2780c184026116d70610",
+    "url": "/chart_sankey_v2/static/js/2.e76a5169.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/chart_sankey_v2/static/js/2.e76a5169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c672249f6f92576e74",
+    "url": "/chart_sankey_v2/static/js/main.721fea97.chunk.js"
+  },
+  {
+    "revision": "b3c5835a59916bf9f9b3",
+    "url": "/chart_sankey_v2/static/js/runtime-main.25411519.js"
+  }
+]);
